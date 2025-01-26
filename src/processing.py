6e2597e9811895927d9cdb7d76f9a45dfcb47644
Andrey Afanasyev <ttrup94@gmail.com>
2025-01-26 20:03:42 +0300

    little update

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Any, Dict, List
 
+
 def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Фильтруем словарь по значению ключа state"""
     return [item for item in data if item.get("state") == state]
