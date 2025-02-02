f3c7f2590a94bce046ef8c6828fd291106630207
Andrey Afanasyev <ttrup94@gmail.com>
2025-02-02 23:18:41 +0300

    add test_masks.py test_widget.py test_processing.py update README.md

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,17 @@
 def get_card_mask_number(number_card: str) -> str:
     """Выводим скрытый номер карты"""
-    mask_card = number_card[:6] + (len(number_card[6:-4]) * "*") + number_card[-4:]
-    mask_split_card = " ".join(mask_card[i * 4 : (i + 1) * 4] for i in range(4))
-
-    return mask_split_card
+    if len(number_card) == 16:
+        mask_card = number_card[:6] + (len(number_card[6:-4]) * "*") + number_card[-4:]
+        mask_split_card = " ".join(mask_card[i * 4 : (i + 1) * 4] for i in range(4))
+        return mask_split_card
+    else:
+        return f"{"Введите 16-ти значный номер карты."}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Выводим скрытый номер аккаунта"""
-    mask_account = (len(account_number[0:2]) * "*") + account_number[-4:]
-
-    return mask_account
+    if len(account_number) == 20:
+        mask_account = (len(account_number[0:2]) * "*") + account_number[-4:]
+        return mask_account
+    else:
+        return f"{"Введите 20-ти значный номер счета."}"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,19 +2,23 @@ import pytest
 
 from src.masks import get_mask_account, get_card_mask_number
 
+
 @pytest.mark.parametrize(
-    "number_card, mask_split_card",
+    "number_card, result",
     [
         ("7000792289606361", "7000 79** **** 6361"),
         ("7158300734726758", "7158 30** **** 6758"),
         ("6831982476737658", "6831 98** **** 7658"),
         ("8990922113665229", "8990 92** **** 5229"),
         ("5999414228426353", "5999 41** **** 6353"),
+        ("", "Введите 16-ти значный номер карты."),
+        ("599941426353", "Введите 16-ти значный номер карты."),
+        ("414228353", "Введите 16-ти значный номер карты."),
     ],
 )
-def test_mask_card_number(number_card: str, mask_split_card: str) -> None:
+def test_mask_card_number(number_card: str, result: str) -> None:
     """Функция передает строку сномером карты"""
-    assert get_card_mask_number(number_card) == mask_split_card
+    assert get_card_mask_number(number_card) == result
 
 
 @pytest.mark.parametrize(
@@ -24,6 +28,8 @@ def test_mask_card_number(number_card: str, mask_split_card: str) -> None:
         ("64686473678894779589", "**9589"),
         ("35383033474447895560", "**5560"),
         ("73654108430135874305", "**4305"),
+        ("", "Введите 20-ти значный номер счета."),
+        ("736541084", "Введите 20-ти значный номер счета."),
     ],
 )
 def test_mask_account(account_number: str, mask_account: str) -> None:
